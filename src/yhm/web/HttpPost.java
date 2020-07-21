4083b51bd8d302c489f64364838ac76df1aa0818
lieyanshu94 <939864132@qq.com>
2020-07-21 17:15:31 +0800

    2020年7月21日 抱歉懒得写

---
+++ src/yhm/web/HttpPost.java
@@ -0,0 +1,50 @@
+package yhm.web;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class HttpPost {
+	protected static final int SOCKET_TIMEOUT = 10000; // 10S
+    protected static final String POST = "POST";
+	public String Post(String host, Map<String, String> params) {
+		try {
+			SSLContext sslcontext = SSLContext.getInstance("TLS");
+            sslcontext.init(null, new TrustManager[] { myX509TrustManager }, null);
+            URL uri = new URL(host); // 创建URL对象
+            HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
+            if (conn instanceof HttpsURLConnection) {
+                ((HttpsURLConnection) conn).setSSLSocketFactory(sslcontext.getSocketFactory());
+            }
+            conn.setConnectTimeout(SOCKET_TIMEOUT); // 设置相应超时
+            conn.setRequestMethod(POST);
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			
+		}
+		return null;
+	}
+	private static TrustManager myX509TrustManager = new X509TrustManager() {
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			return null;
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+	};
+}
